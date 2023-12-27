e650fa1805bb20f6191185d3bbc591b6d272d9d8
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-27 17:56:24 +0000

    Add tsconfig-replace-paths
    This dependency was added to fix the issue with Node.js not finding any
    imports that started with the "@" path defined in tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { sequelize } from "./classes/db";
+import { sequelize } from "@/classes/db";
 
 import "@/strategies/localStrategy";
 
