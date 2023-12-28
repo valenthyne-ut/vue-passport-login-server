54b94c119bdd4aabb04f71151d449feb0cff711d
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-28 14:58:12 +0000

    Change response status of Auth api from 500 to 501

--- src/api/Auth.api.ts
+++ src/api/Auth.api.ts
@@ -1,5 +1,5 @@
 import { app } from "@/index";
 
 app.post("/api/auth", (request, response) => {
-	response.status(500).send("Not implemented.");
+	response.status(501).send("Not implemented.");
 });
