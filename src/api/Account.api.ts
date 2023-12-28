2bf50781317609ed81b704910f23b18042899e71
valenthyne-ut <valenthyne.ut@gmail.com>
2023-12-28 17:08:42 +0000

    Account creation endpoint implementation

--- src/api/Account.api.ts
+++ src/api/Account.api.ts
@@ -1,8 +1,24 @@
+import { User } from "@/classes/db/models/User.model";
+import { hashSync } from "bcrypt";
+import { randomBytes } from "crypto";
 import { Router } from "express";
 
 const accountRouter = Router()
 	.post("/", (request, response) => {
-		response.status(501).send("Not implemented.");
+		const randomUsername = randomBytes(4).toString("hex");
+		const randomPassword = randomBytes(8).toString("hex");
+
+		User.create({
+			name: randomUsername,
+			password_hash: hashSync(randomPassword, 11)
+		}).then(() => {
+			return response.status(201).json({
+				username: randomUsername,
+				password: randomPassword
+			});
+		}).catch(() => {
+			return response.status(500).send("Couldn't create new user.");
+		});
 	});
 
 export { accountRouter };
